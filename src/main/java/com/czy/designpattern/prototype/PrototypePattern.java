f68432df2177bcae0fb3cb1af11798db945f785c
Banjalore <1531837464@qq.com>
2026-05-12 22:32:46 +0800

    feat: 原型模式

---
+++ src/main/java/com/czy/designpattern/prototype/PrototypePattern.java
@@ -0,0 +1,44 @@
+package com.czy.designpattern.prototype;
+/***
+ * 原型模式
+ */
+public class PrototypePattern {
+    public static void main(String argsp[]){
+        Product product1 = new Product(1, 10.00);
+        System.out.println(product1.getId() + " " + product1.getPrice());
+
+        Product product2 = (Product) product1.Clone();
+        System.out.println(product2.getId() + " " + product2.getPrice());
+    }
+}
+
+interface Prototype{
+    public Object Clone();
+}
+
+class Product implements Prototype{
+    private int id;
+    private double price;
+
+    public Product(){};
+
+    public Product(int id, double price){
+        this.id = id;
+        this.price = price;
+    }
+    public int getId(){
+        return id;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    @Override
+    public Object Clone() {
+        Product product = new Product();
+        product.id = this.id;
+        product.price = this.price;
+        return product;
+    }
+}
